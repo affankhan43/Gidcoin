f974527712bd21b9f04051957c548d007aef7f7a
affankhan43 <affankhan627@gmail.com>
2018-12-19 01:56:02 +0500

    config

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -12,7 +12,7 @@ namespace parameters {
 
 const uint64_t CRYPTONOTE_MAX_BLOCK_NUMBER                   = 500000000;
 const size_t   CRYPTONOTE_MAX_BLOCK_BLOB_SIZE                = 500000000;
-const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 2000000000;
+const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 1000000000;
 //TODO Currency-specific address prefix
 const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 0x5e1f; //24095
 //TODO Choose maturity period for your currency
@@ -24,7 +24,7 @@ const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
 //TODO Specify total number of available coins
 //TODO ((uint64_t)(-1)) equals to 18446744073709551616 coins
 //TODO or you can define number explicitly UINT64_C(858986905600000000)
-const uint64_t MONEY_SUPPLY                                  = UINT64_C(500000000000);
+const uint64_t MONEY_SUPPLY                                  = UINT64_C(900000000000);
 const unsigned EMISSION_SPEED_FACTOR                         = 18;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "GidCoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff000101029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101c550f3ba7a6475dcf92fd08451a01194b8d6331174dafa54154976585b5d9ecf";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
--- src/CryptoNoteCore/Currency.cpp
+++ src/CryptoNoteCore/Currency.cpp
@@ -105,14 +105,23 @@ bool Currency::getBlockReward(size_t medianSize, size_t currentBlockSize, uint64
   if (alreadyGeneratedCoins == 0) {
       baseReward = 1;
   }
-
   if (alreadyGeneratedCoins == 1) {
-      baseReward =(m_moneySupply*1)-1;
+      baseReward =m_moneySupply*0.10;
   }
-
   if (alreadyGeneratedCoins + baseReward >= m_moneySupply) {
       baseReward = 0;
   }
+  // if (alreadyGeneratedCoins == 0) {
+  //     baseReward = 1;
+  // }
+
+  // if (alreadyGeneratedCoins == 1) {
+  //     baseReward =(m_moneySupply*1)-1;
+  // }
+
+  // if (alreadyGeneratedCoins + baseReward >= m_moneySupply) {
+  //     baseReward = 0;
+  // }
 
 
   medianSize = std::max(medianSize, m_blockGrantedFullRewardZone);
